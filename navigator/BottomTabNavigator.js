2bd1b9d2037784ff4cf355c859ecaa81348ac4bc
Phatphum Tarsaku <phumfirebase@gmail.com>
2023-07-08 22:06:47 +0700

    added bottom tab icons

--- navigator/BottomTabNavigator.js
+++ navigator/BottomTabNavigator.js
@@ -5,18 +5,34 @@ import HomeScreen from '../screens/HomeScreen'
 import TripScreen from '../screens/TripScreen'
 import DiaryScreen from '../screens/DiaryScreen'
 import { Foundation } from '@expo/vector-icons';
-import { Svg } from 'react-native-svg'
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator()
 
 export default function BottomTabNavigator() {
     return (
-        <Tab.Navigator initialRouteName='Home' screenOptions={{ tabBarActiveTintColor: "#3D4267", tabBarInactiveTintColor: "#d9d9d9" }}>
+        <Tab.Navigator initialRouteName='Home' screenOptions={{ tabBarActiveTintColor: "#3D4267", tabBarInactiveTintColor: "#d9d9d9", tabBarLabelStyle: { fontFamily: } }}>
             <Tab.Screen name='Home' component={HomeScreen} options={{
-                title: 'Home', headerShown: false
+                title: 'Home', headerShown: false, tabBarIcon: (tabInfo) => {
+                    return (
+                        <Foundation name="home" size={24} color={tabInfo.focused ? "#3D4267" : "#D9D9D9"} />
+                    );
+                },
             }} />
-            <Tab.Screen name='Trips' component={TripScreen} options={{ title: 'Trips', headerShown: false }} />
-            <Tab.Screen name='Diaries' component={DiaryScreen} options={{ title: 'Diaries', headerShown: false }} />
+            <Tab.Screen name='Trips' component={TripScreen} options={{
+                title: 'Trips', headerShown: false, tabBarIcon: (tabInfo) => {
+                    return (
+                        <FontAwesome5 name="clipboard-check" size={24} color={tabInfo.focused ? "#3D4267" : "#D9D9D9"} />
+                    );
+                }
+            }} />
+            <Tab.Screen name='Diaries' component={DiaryScreen} options={{
+                title: 'Trips', headerShown: false, tabBarIcon: (tabInfo) => {
+                    return (
+                        <FontAwesome5 name="book" size={24} color={tabInfo.focused ? "#3D4267" : "#D9D9D9"} />
+                    );
+                }
+            }} />
         </Tab.Navigator>
     )
 }
